a6f9cb5cfc0d6a37b5d024927a19e0191cdb6675
toster <tostercx@gmail.com>
2020-12-26 15:32:26 +0200

    fix clearing

--- start_and_clear_save.bat
+++ start_and_clear_save.bat
@@ -1,3 +1,3 @@
 @echo off
-del "%appdata%\Macromedia\Flash Player\#SharedObjects\EX5BBAJC\localhost\upixo.sol"
+del /Q /S "%appdata%\Macromedia\Flash Player\#SharedObjects"
 start flashplayer_32_sa game_data\misnowdriftland_final.swf
